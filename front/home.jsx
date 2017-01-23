8a3a77df719929a2e589c023971dd454e53f6961
Gabemb <gabrielbny@gmail.com>
2017-01-23 12:09:56 -0500

    update

--- front/home.jsx
+++ front/home.jsx
@@ -14,12 +14,11 @@ const Home = withRouter(React.createClass({
            
              <div style={{width: '100vw', height: '100vh'}} data-vide-bg={require("./images/Underground-Traffic.mp4")} data-vide-options=" position: 0% 0%"></div>
              <div className="overlay">
-                <center>  <h1 id="welcome">Welcome to Dream Land</h1> </center>
+                <center>  <h1 id="welcome">WELCOME TO DREAM LAND</h1> </center>
                  <center> <SearchBar /> </center>
              </div>
        </div>
         
-   <i class="fa fa-heart-o" aria-hidden="false"></i> <i class="fa fa-heart" aria-hidden="true"></i>
 
 
     </div>
